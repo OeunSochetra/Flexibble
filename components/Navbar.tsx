72720517a266ec8ce1181cb9c35981f91b8a30a3
OeunSochetra <oeunsochetra@gmail.com>
2023-07-12 17:10:44 +0700

    update

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,35 +2,45 @@ import Link from "next/link";
 import Image from "next/image";
 import { NavLinks } from "@/constant";
 import AuthProviders from "./AuthProviders";
+import { getCurrentUser } from "@/lib/session";
 
-const Navbar = () => {
-    const session ={};
+const Navbar = async () => {
+    const session = await getCurrentUser();
   return (
-    <nav className="flexBetween navbar">
-      <div className="flex-1 flexStart gap-10">
-        <Link href="/">
-          <Image src="/logo.svg" width={116} height={43} alt="logo"></Image>
-          <ul className="xl:flex hidden text-sm gap-7">
-            {NavLinks.map((link) => (
-              <Link href={link.href} key={link.text}>
-                {link.text}
-              </Link>
-            ))}
-          </ul>
-        </Link>
-      </div>
-      <div className="flexCenter gap-4" >
-        {session ? (
-          <>
-            UserPhotos
-            <Link href="create-project">ShareWork</Link>
-          </>
-        ) : (
-          <AuthProviders></AuthProviders>
-        )}
-      </div>
-    </nav>
-  );
+    <nav className='flexBetween navbar'>
+    <div className='flex-1 flexStart gap-10'>
+      <Link href='/'>
+        <Image
+          src='/logo.svg'
+          width={116}
+          height={43}
+          alt='logo'
+        />
+      </Link>
+      <ul className='xl:flex hidden text-small gap-7'>
+        {NavLinks.map((link) => (
+          <Link href={link.href} key={link.text}>
+            {link.text}
+          </Link>
+        ))}
+      </ul>
+    </div>
+
+    <div className='flexCenter gap-4'>
+      {session?.user ? (
+        <>
+          <ProfileMenu session={session} />
+
+          <Link href="/create-project">
+            <Button title='Share work' />
+          </Link>
+        </>
+      ) : (
+        <AuthProviders />
+      )}
+    </div>
+  </nav>
+);
 };
 
 export default Navbar;
